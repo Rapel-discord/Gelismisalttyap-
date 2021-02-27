d8fbaedd9a26bfd72f172374d38c7e0e9a87a3aa
Rapel <79516185+Rapel-discord@users.noreply.github.com>
2021-02-27 09:37:15 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Gelismisalttyap-
+Bot Altyapı Projesine Hoşgeldin!
+=================
+Merhaba Arkadaşlar Bu Altyapı 2018 Yılında Yapılmış 2021 Yılında İse Gerekli Düzeltmeleri Yapılmıştır. Bu Altyapı Discord.js v12 Sürümünü Artık Desteklemektedir! Kolaylıkla Kullanabilirsiniz!
+
---
+++ komutlar/yardım.js
@@ -0,0 +1,109 @@
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json') 
+
+exports.run = async (client, message, args) => {
+    var prefix = ayarlar.prefix;
+
+//GENEL KOMUTU
+    if(args[0] === "Genel" || args[0] === "genel" || args[0] === "General" || args[0] === "general") {
+              let Genel = new Discord.MessageEmbed()
+  .setAuthor('Genel', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Genel').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+        .addField("» Linkler", ` [Davet Et](LİNK)` + "** | **" + `[Destek Sunucusu](LİNK)`  + "** | **" + `[Oy Ver](LİNK)`  + "** | **" + `[Web Sitesi](https://gnarge.xyz/)  `, false)
+              return message.channel.send(Genel)
+         
+       
+       return;
+    }
+    //SUNUCU KOMUTU
+      if(args[0] === "Sunucu" || args[0] === "sunucu") {
+              let Sunucu = new Discord.MessageEmbed()
+  .setAuthor('Sunucu', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Sunucu').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+        .addField("» Linkler", ` [Davet Et](LİNK)` + "** | **" + `[Destek Sunucusu]LİNK)`  + "** | **" + `[Oy Ver](LİNK)`  + "** | **" + `[Web Sitesi](LİNK)  `, false)
+              return message.channel.send(Sunucu)
+         
+      
+
+       return;
+    }
+//EĞLENCE KOMUTU
+  if(args[0] === "Eğlence" || args[0] === "eğlence" || args[0] === "Fun" || args[0] === "fun") {
+   let Eğlence = new Discord.MessageEmbed()
+  .setAuthor('Eğlence', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Eğlence').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+        .addField("» Linkler", ` [Davet Et](LİNK)` + "** | **" + `[Destek Sunucusu](LİNK)`  + "** | **" + `[Oy Ver](https://bit.ly/3980hKq)`  + "** | **" + `[Web Sitesi](https://gnarge.xyz/)  `, false)
+   return message.channel.send(Eğlence)
+  
+      
+           return;
+  }
+  //MODERASYON KOMUTU
+  if(args[0] === "Moderasyon" || args[0] === "moderasyon" || args[0] === "moderation" || args[0] === "Moderation") {
+   let Moderasyon = new Discord.MessageEmbed()
+  .setAuthor('Moderasyon', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Moderasyon').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+        .addField("» Linkler", ` [Davet Et](Link)` + "** | **" + `[Destek Sunucusu](LİNK)`  + "** | **" + `[Oy Ver](LİNK)`  + "** | **" + `[Web Sitesi](LİNK)  `, false)
+   return message.channel.send(Moderasyon)
+             
+       
+               return;
+  }
+     //SAHİP KOMUTU
+  if(args[0] === "Sahip" || args[0] === "sahip" ) {
+    let Sahip = new Discord.MessageEmbed()
+   .setAuthor('Moderasyon', message.author.displayAvatarURL())
+   .setColor('#2667FF')
+   .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Sahip').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+         .addField("» Linkler", ` [Davet Et](Kİ)` + "** | **" + `[Destek Sunucusu](https://discord.gg/2vPdmYz)`  + "** | **" + `[Oy Ver](https://bit.ly/3980hKq)`  + "** | **" + `[Web Sitesi](https://gnarge.xyz/)  `, false)
+    return message.channel.send(Sahip)
+              
+        
+                return;
+   }
+
+//YARDIM KOMUTU
+  
+  let embed = new Discord.MessageEmbed()
+  .setAuthor('Yardım Komutları', message.author.displayAvatarURL())
+  .setThumbnail(client.user.avatarURL())
+  .setColor('#FFFB05')
+  .setDescription(`**Örnek Kullanım:** \`${prefix}yardım Kategori\` \n **Örnek:** \`${prefix}yardım Genel\``)
+  .addField('Kategoriler:', `
+  **[${prefix}yardım Genel](Sunucu Link)** 
+  **[${prefix}yardım Eğlence](Sunucu Link)**
+  **[${prefix}yardım Moderasyon](Sunucu Link)**
+  **[${prefix}yardım Sunucu](Sunucu Link)**
+  **[${prefix}yardım Sahip](Sunucu Link)**
+  `)
+  .addField("» Linkler", ` [Davet Et](LİNK)` + "** | **" + `[Destek Sunucusu](LİNK)`  + "** | **" + `[Oy Ver](LİNK)`  + "** | **" + `[Web Sitesi](LİNK)  `, false)
+
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+  message.channel.send(embed)
+
+  
+}
+    
+//Rapel Tarafından Yenılenmıstır
+
+  exports.conf = {
+    aliases: ['help', 'cmds', 'komutlar','y'], //Komutun farklı yazılışlarla kullanımları
+    permLevel: 0, //Komutun kimler kullanacağını belirtir (bot.js dosyasından en aşağı inerseniz gerekli yeri görürsünüz)
+    kategori: "Genel" //Yardım komutunda hangi kategoride gözükeceğini ayarlarsınız
+
+  };
+
+  exports.help = {
+    name: 'yardım',  //adını belirtin (kullanmak için gereken komut) Örneğin Otorol
+    description: 'Komutlar hakkında bilgi verir.', //Komutun açıklaması
+    usage: 'yardım', //Komutun kullanım şekli (örneğin !otorol <@rol> <#kanal>)
+  };
